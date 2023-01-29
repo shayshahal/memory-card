5c3a0d44f6c22b96f1e8191184361b5b42bb1493
shayshahal <shay840@gmail.com>
2023-01-30 00:06:05 +0200

    Component layout setup and file adding

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-import './App.css';
+import '../styles/App.css';
 
-function App() {
+export default function App() {
 
   return (
     <div className="App"> 
@@ -8,5 +8,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
