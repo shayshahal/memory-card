f2d099b65f65290449f2adc3f614002d64e85675
shayshahal <shay840@gmail.com>
2023-01-30 00:23:08 +0200

    add state for app

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,12 @@ import { useState } from 'react';
 import '../styles/App.css';
 
 export default function App() {
-  const [isManyGame, gameToggler] = useToggle({})
-
-
+  const [isManyGame, setIsManyGame] = useState(true);
+  const [isDifficultEasy, setIsDifficultEasy] = useState(true);
+  const [isSettingsOpen, setIsSettingsOpen] = useState(true);
   return (
     <div className="App"> 
+    
     </div>
   );
 }
