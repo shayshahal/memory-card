4b88ef78698de2cfc52ee01bc233a629dafb0489
shayshahal <shay840@gmail.com>
2023-01-30 00:16:37 +0200

    add useToggle hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
+import { useState } from 'react';
 import '../styles/App.css';
+
+function useToggle({firstFn, secondFn}){
+  const [isFirst, setIsFirst] = useState(true);
+  function toggler(){
+    const next = !isFirst;
+    setIsFirst(next);
+    next ? firstFn() : secondFn();
+  }
+  return [isFirst, toggler];
+}
 
 export default function App() {
 
+
   return (
     <div className="App"> 
-      Hi
     </div>
   );
 }
