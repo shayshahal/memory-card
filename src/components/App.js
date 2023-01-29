3247ca218fd8c2f968fae774cd39bb30b0548e7c
shayshahal <shay840@gmail.com>
2023-01-30 00:21:21 +0200

    remove useToggle hook, unneeded

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,8 @@
 import { useState } from 'react';
 import '../styles/App.css';
 
-function useToggle({firstFn, secondFn}){
-  const [isFirst, setIsFirst] = useState(true);
-  function toggler(){
-    const next = !isFirst;
-    setIsFirst(next);
-    next ? firstFn() : secondFn();
-  }
-  return [isFirst, toggler];
-}
-
 export default function App() {
+  const [isManyGame, gameToggler] = useToggle({})
 
 
   return (
