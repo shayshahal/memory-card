1a658efb1fc389765b5b9cc791536060f10858bd
shayshahal <shay840@gmail.com>
2023-01-30 20:47:30 +0200

    fixed img url

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,8 @@ import '../styles/Card.css'
 export default function Card(props){
     return (
         <button onClick={props.onClick} className='Card'>
-            <img alt={props.champ.name} src={`ddragon.leagueoflegends.com/cdn/img/champion/loading/${props.champ.name}_${props.champ.name}.jpg`}/>
-            <span>{props.champ.name}, {props.champ.name}</span>
+            <img alt={props.champ.name} src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${props.champ.name}_${props.champ.skin}.jpg`}/>
+            <span>{props.champ.name}, {props.champ.title}</span>
         </button>
     )
 }
