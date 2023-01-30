2669b664f104e57c0256b3d012e4270c86e46aec
shayshahal <shay840@gmail.com>
2023-01-30 18:11:06 +0200

    changed card props to simplify

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,8 @@ import '../styles/Card.css'
 export default function Card(props){
     return (
         <button onClick={props.onClick} className='Card'>
-            <img alt={props.name} src={`ddragon.leagueoflegends.com/cdn/img/champion/loading/${props.name}_${props.skin}.jpg`}/>
-            <span>{props.name}, {props.title}</span>
+            <img alt={props.champ.name} src={`ddragon.leagueoflegends.com/cdn/img/champion/loading/${props.champ.name}_${props.champ.name}.jpg`}/>
+            <span>{props.champ.name}, {props.champ.name}</span>
         </button>
     )
 }
