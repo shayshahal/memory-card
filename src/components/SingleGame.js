8f0a8dc5d506187d801370dbb131acb4dedf67f9
shayshahal <shay840@gmail.com>
2023-01-30 18:11:28 +0200

    finished single game

---
+++ src/components/SingleGame.js
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import '../styles/SingleGame.css'
+import Card from './Card';
+import Score from './Score';
+
+export default function SingleGame(props){
+    const [currentChamp, setCurrentChamp] = useState(getRandomChamp())
+    const [memory, setMemory] = useState(new Set());
+    const [score, setScore] = useState(0);
+    function getRandomChamp(){
+        return props.champPull[~~(Math.random()*props.champPull.length)]
+    }
+    function lose(){
+        setScore(0);
+        setMemory(new Set());
+    }
+    function win(){
+        setScore(prev=>prev+1);
+        setMemory(prev=>prev.add(currentChamp))
+    }
+    function choiceHandler(isSeen){
+        if(isSeen === memory.has(currentChamp))
+            win();
+        else
+            lose();
+        setCurrentChamp(getRandomChamp());
+    }
+    return (
+        <div className={'SingleGame'}>
+        <Card champ={currentChamp}/>
+        <button onClick={()=>{choiceHandler(true)}}>Seen</button>            
+        <button onClick={()=>{choiceHandler(false)}}>New</button>
+        <Score score={score}/>
+        </div>
+    )
+}
