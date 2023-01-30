a07f064e5f35d7349a919139aa1d97637df18181
shayshahal <shay840@gmail.com>
2023-01-30 20:48:46 +0200

    added fetch call on mount

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,44 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import '../styles/App.css';
+import ManyGame from './ManyGame';
 import Settings from './Settings';
+import SingleGame from './SingleGame';
+
+function transformObject(obj, isHard = false){
+  let res = [];
+  for (let i = 0; i < (isHard?obj.skins.length:1); i++) {
+    res.push({
+        name: obj.name,
+        key: obj.key,
+        title: obj.title,
+        skin: obj.skins[i].num
+      }
+    )
+  }
+  return res;
+}
 
 export default function App() {
   const [isSingleGame, setIsSingleGame] = useState(false);
-  const [isDifficultyHard, setIsDifficultyHard] = useState(false);
-
+  const [champPull, setChampPull] = useState([]);
+  const [isDifficultyHard, setIsDifficultyHard] = useState(false)
+  useEffect(() => {
+    const controller = new AbortController();
+    async function fetchChamps(){
+      const response = await fetch('https://ddragon.leagueoflegends.com/cdn/13.1.1/data/en_US/championFull.json', {signal: controller.signal});
+      const data = await response.json();
+      let res = [];
+      for(const v of Object.values(data.data)){
+        res = [...res, ...transformObject(v)]
+      }
+      setChampPull(res);
+    }
+    fetchChamps();
+    return () => {
+      controller.abort();
+    }
+  },[])
+  
   return (
     <div className="App"> 
       <header>
@@ -15,6 +48,12 @@ export default function App() {
           setIsDifficulty={()=>{setIsDifficultyHard(!isDifficultyHard)}}
         />
       </header>
+      {isSingleGame 
+        ?
+          (champPull.length && <SingleGame champPull={champPull}/>)
+        :
+          (champPull.length && <ManyGame champPull={champPull}/>)
+      }
     </div>
   );
 }
